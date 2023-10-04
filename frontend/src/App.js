ecfd52c11a6e52ec33e7bde103eae058a1c0978c
Jiajun Dai <jdai10@mail.ccsf.edu>
2023-10-04 12:33:59 -0700

    fixed path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/header";
-import Home from "./Pages/Home";
-import Test from "./Pages/Test";
+import Home from "./pages/Home";
+import Test from "./pages/Test";
 
 
 export default function App() {
