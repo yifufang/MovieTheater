42559c886db9c40f4ed9d82f92d01c9ae2b4702f
yifufang <yffang01@gmail.com>
2023-10-08 01:03:42 -0700

    updating homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
+import CarouselDefault from "../components/Carousel/CarouselDefault";
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <h1 className="text-4xl font-bold text-center">
-        Hi! Alpha Team. You have arrived at the Home Page.
-      </h1>
+    <div>
+      <CarouselDefault />
     </div>
+
   );
 }
