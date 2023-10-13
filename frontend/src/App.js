6534b94fba7b364c41c0f7f0e972636c1664e77a
yifufang <yffang01@gmail.com>
2023-10-13 11:27:43 -0700

    authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,51 +18,6 @@ import Test from "./pages/Test";
 
 export default function App() {
 
-  const [currentForm, setCurrentForm] = useState("login");
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName)
-  }
-
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/regsiter" element={<Register />} />
-        <Route path="/test" element={<Test />} />
-      </Routes>
-    </div>
-  );
-}
-
-/*
-import React, { useState } from "react";
-import { Routes, Route, useLocation, Outlet} from "react-router-dom";
-import { Link } from 'react-router-dom';
-import "./App.css";
-import { Login } from "./pages/Login"
-import { Signup } from "./pages/Signup"
-
-//pagelayout component contains the header and footer that apears on all pages of under / and its children
-import Pagelayout from "./components/Pagelayout/Pagelayout";
-
-//import chidlren pages of /
-import Membership from "./pages/Membership";
-import ContactUs from "./pages/ContactUs";
-import AboutUs from "./pages/AboutUs";
-import Home from "./pages/Home";
-import Test from "./pages/Test";
-
-
-export default function App() {
-
-  const[currentForm, setCurrentForm] = useState("login");
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName)
-  }
-
   return (
     <div className="overflow-hidden">
       <Routes>
@@ -75,10 +30,9 @@ export default function App() {
         </Route>
         <Route>
           <Route path="/auth/login" element={<Login />} />
-          <Route path="/auth/signup" element={<Signup />} />
+          <Route path="/auth/Register" element={<Register />} />
         </Route>
       </Routes>
     </div>
   );
 }
-*/
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Statistics from "../components/Statistics/Statistics";
 export default function Home() {
   return (
     <div className="">
-      <h1>Hi! Alpha Team.</h1>
       <CarouselDefault />
       <Statistics />
     </div>
