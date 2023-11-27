ba1cb38528e52d9c01d6563ab3f4e45817c7ecd1
yifufang <yffang01@gmail.com>
2023-11-27 01:07:55 -0800

    Revert "gitignore"
    This reverts commit b94edb0ff38340a07e9509fcca2811e6e072dbf8.

--- frontend/src/components/Pagelayout/Pagelayout.jsx
+++ frontend/src/components/Pagelayout/Pagelayout.jsx
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer";
-export default function PageLayout({ children }) {
+export default function Pagelayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
