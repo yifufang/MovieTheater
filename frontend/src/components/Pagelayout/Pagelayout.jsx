6d4c797033378c2d97a0de44dc4094648aaec4ce
yifufang <yffang01@gmail.com>
2023-11-27 11:18:50 -0800

    authentication

--- frontend/src/components/Pagelayout/Pagelayout.jsx
+++ frontend/src/components/Pagelayout/Pagelayout.jsx
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer";
-export default function Pagelayout({ children }) {
+export default function PageLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
