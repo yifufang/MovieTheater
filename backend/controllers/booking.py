c4e3a647f4cadb78000034910182df077acce274
yifufang <yffang01@gmail.com>
2023-11-30 22:38:02 -0800

    changes

--- backend/controllers/booking.py
+++ backend/controllers/booking.py
@@ -11,35 +11,35 @@ def get_all_theaters_name_id():
 
 def get_movies_info_for_theater_A():
     cur = app.mysql.connection.cursor()
-    cur.execute("SELECT title, movies.film_id, thumbnail FROM movies.schedules INNER JOIN movies ON movies.film_id = schedules.film_id WHERE theater_id = %s group by movies.film_id", ('a',))
+    cur.execute("SELECT title, movies.film_id, thumbnail FROM movies.film_schedules INNER JOIN movies ON movies.film_id = film_schedules.film_id WHERE theater_id = %s group by movies.film_id", ('a',))
     data = cur.fetchall()
     cur.close()
     return data
 
 def get_movies_info_for_theater_B():
     cur = app.mysql.connection.cursor()
-    cur.execute("SELECT title, movies.film_id, thumbnail FROM movies.schedules INNER JOIN movies ON movies.film_id = schedules.film_id WHERE theater_id = %s group by movies.film_id", ('b',))
+    cur.execute("SELECT title, movies.film_id, thumbnail FROM movies.film_schedules INNER JOIN movies ON movies.film_id = film_schedules.film_id WHERE theater_id = %s group by movies.film_id", ('b',))
     data = cur.fetchall()
     cur.close()
     return data
 
 def get_movies_info_for_theater_C():
     cur = app.mysql.connection.cursor()
-    cur.execute("SELECT title, movies.film_id, thumbnail FROM movies.schedules INNER JOIN movies ON movies.film_id = schedules.film_id WHERE theater_id = %s group by movies.film_id", ('c',))
+    cur.execute("SELECT title, movies.film_id, thumbnail FROM movies.film_schedules INNER JOIN movies ON movies.film_id = film_schedules.film_id WHERE theater_id = %s group by movies.film_id", ('c',))
     data = cur.fetchall()
     cur.close()
     return data
 
 def get_movies_info_for_theater_D():
     cur = app.mysql.connection.cursor()
-    cur.execute("SELECT title, movies.film_id, thumbnail FROM movies.schedules INNER JOIN movies ON movies.film_id = schedules.film_id WHERE theater_id = %s group by movies.film_id", ('d',))
+    cur.execute("SELECT title, movies.film_id, thumbnail FROM movies.film_schedules INNER JOIN movies ON movies.film_id = film_schedules.film_id WHERE theater_id = %s group by movies.film_id", ('d',))
     data = cur.fetchall()
     cur.close()
     return data
 
 def get_film_schedules_given_theaterID_filmID(theater_id, movie_id):
     cur = app.mysql.connection.cursor()
-    cur.execute("SELECT start_time FROM schedules WHERE theater_id = %s AND film_id = %s", (theater_id, movie_id,))
+    cur.execute("SELECT start_time FROM film_schedules WHERE theater_id = %s AND film_id = %s", (theater_id, movie_id,))
     data = cur.fetchall()
     cur.close()
     output = []
